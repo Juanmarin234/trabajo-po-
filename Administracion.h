2c9da98fb5172441d6a158345d991da0f28ea8e4
Juanmarin234 <jpuanxx23@gmail.com>
2026-04-20 23:04:57 -0500

    Add files via upload

---
+++ Administracion.h
@@ -0,0 +1,50 @@
+#ifndef ADMINISTRACION_H
+#define ADMINISTRACION_H
+
+#include <iostream>
+#include "Propietario.h"
+#include "Propiedad.h"
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::getline;
+using std::string;
+using std::vector;
+
+class Administracion
+{
+private:
+    float cobroAscensor;
+    float costoBase;
+    float recargo;
+    int contIds;
+    vector<Propietario *> propietarios;
+    vector<Propiedad *> propiedades;
+
+    void inicializarDatos();
+
+public:
+    Administracion();
+
+    virtual ~Administracion();
+
+    void imprimirPropietariosConParqueadero();
+
+    void imprimirUnPropietario(double id);
+
+    void imprimirPropietarios();
+
+    void imprimirPropietariosSinCuarto();
+
+    void imprimirPropietariosCuartoUtil(bool isTerminado); // Ojo un solo método sirve para las dos funciones.
+    void recaudarAdministracion();
+
+    void agregarPropiedad();
+
+    void agregarPropietario();
+
+    void relacionarPropietarioPropiedad();
+};
+
+#endif
---
+++ po.py
@@ -0,0 +1,7 @@
+def nombre():
+    nombre = input("ingrese su nombre: ")
+    if nombre.lower() == "carlos":
+        print("usted es el ganador !!!")
+    else:
+        print("tu nombre podria ser mejor !!!")
+    nombre
